d97ce30a5d8777c089ebd82f6edd4d1b4a96f962
abdurrazzakhera <hhera1413@gmail.com>
2022-03-31 11:38:14 +0600

    set origin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <h3>dhklajgsklajsdfgkla</h3>
       <Shop></Shop>
     </div>
   );
