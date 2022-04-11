0ab7e51fb12403bae6fd189f30d81f49d62acfbb
abdurrazzakhera <hhera1413@gmail.com>
2022-04-11 21:12:05 +0600

    login page workin

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,62 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 import "./Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const navigate = useNavigate();
+
+  const handelEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+  const handelPasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    signInWithEmailAndPassword(email, password);
+    console.log(user);
+  };
+  if (user) {
+    navigate("/");
+  }
+
   return (
     <div className='form-container'>
       <div>
         <h2 className='form-title'>Login</h2>
-        <form action=''>
+        <form onSubmit={handleLogin}>
           <div className='input-group'>
             <label htmlFor=''>Email</label>
-            <input type='email' name='' id='' required />
+            <input
+              onBlur={handelEmailBlur}
+              type='email'
+              name=''
+              id=''
+              required
+            />
           </div>
           <div className='input-group'>
             <label htmlFor=''>Password</label>
-            <input type='password' name='' id='' required />
+            <input
+              onBlur={handelPasswordBlur}
+              type='password'
+              name=''
+              id=''
+              required
+            />
           </div>
+          <p>{loading && "loadding ..."}</p>
+          <p>Signed In User: {user?.email}</p>
+          <p>{error?.message}</p>
           <input className='form-submit' type='submit' value='Login' />
         </form>
         <p className='signup-para'>
